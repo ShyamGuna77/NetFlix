743fcc9964c851a61c2cad914becfeec18b5fe3c
Shyam <gshyamprasad2001@gmail.com>
2025-02-14 16:07:06 +0530

    Login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@
 
 const Header = () => {
     return (
-        <div className="absolute w-full left-0 top-0 px-28 py-4 bg-gradient-to-b from-gray-900 flex items-center ">
+        <div className="absolute z-10 w-full left-0 top-0 px-28 py-4 bg-gradient-to-b from-gray-900 flex items-center ">
             <img
-                className="w-48 opacity-100 "
+                className="w-48  "
                 src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
                 alt="Netflix"
-                style={{ letterSpacing: '2px' }}
+            
             />
         </div>
     );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,85 @@
-import Header from "./Header"
-
+import { useState } from "react";
+import Header from "./Header";
 
 const Login = () => {
-return (
-    <div>
-        <Header />
-        <div className="relative">
-            <img
-                className="opacity-50"
-                src="https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_large.jpg"
-                alt="Background asset"
-            />
-            <div className="absolute inset-0 bg-black opacity-70"></div>
-        </div>
-    </div>
-);
-}
+    const [signIn, setSignIn] = useState(true);
 
-export default Login
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    function togglechange() {
+        setSignIn(!signIn);
+    }
+
+    return (
+        <div className="relative min-h-screen ">
+            <Header />
+            <div className="absolute w-full h-full">
+                <img
+                    className="w-full h-full object-cover brightness-50"
+                    src="https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_large.jpg"
+                    alt="Background asset"
+                />
+            </div>
+
+            <form
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+                                bg-black/50 p-12 rounded-lg w-96"
+                onSubmit={handleSubmit}
+            >
+                <h1 className="text-white text-3xl font-bold mb-8">
+                    {signIn ? "Sign In" : "Sign Up"}
+                </h1>
+                {!signIn && (
+                    <>
+                        <input
+                            className="w-full bg-gray-700 text-white p-4 my-4 rounded-md 
+                                        focus:outline-none focus:ring-2 focus:ring-red-600"
+                            type="text"
+                            placeholder="First Name"
+                            id="firstName"
+                        />
+                        <input
+                            className="w-full bg-gray-700 text-white p-4 my-4 rounded-md 
+                                        focus:outline-none focus:ring-2 focus:ring-red-600"
+                            type="text"
+                            placeholder="Last Name"
+                            id="lastName"
+                        />
+                    </>
+                )}
+                <input
+                    className="w-full bg-gray-700 text-white p-4 my-4 rounded-md 
+                                        focus:outline-none focus:ring-2 focus:ring-red-600"
+                    type="text"
+                    placeholder="Enter Email"
+                    id="email"
+                />
+                <input
+                    className="w-full bg-gray-700 text-white p-4 my-4 rounded-md 
+                                        focus:outline-none focus:ring-2 focus:ring-red-600"
+                    type="password"
+                    placeholder="Enter Password"
+                    id="password"
+                />
+                <button
+                    className="w-full bg-red-600 text-white py-2 mt-6 rounded-md 
+                                        hover:bg-red-700 font-bold transition duration-200"
+                >
+                    {signIn ? "Sign In" : "Sign Up"}
+                </button>
+                <p className="text-white text-center m-4">Or</p>
+                <button
+                    onClick={togglechange}
+                    className="w-full bg-[#362F2D] text-white py-2 mt-3 rounded-md 
+                                        hover:bg-red-700 font-bold transition duration-200"
+                >
+                    {signIn ? "Sign Up" : "Sign In"}
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
